9453a90397f5d1716dcef4dce165c3273ff1b1c2
agusbarralia <140986863+agusbarralia@users.noreply.github.com>
2024-08-20 11:51:48 -0300

    Add files via upload

---
+++ src/main/java/com/elixir/elixir/ElixirApplication.java
@@ -0,0 +1,13 @@
+package com.elixir.elixir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElixirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElixirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elixir/elixir/ElixirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixir.elixir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElixirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
