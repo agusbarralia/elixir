2f76d70e7852a395a250235683186f8a4db3ee23
Leivon8899 <lleiva@uade.edu.ar>
2024-09-06 10:44:26 -0300

    Order Controller y Service cambios

--- src/main/java/com/elixir/elixir/exceptions/OrderNoSuchElementException.java
+++ src/main/java/com/elixir/elixir/exceptions/OrderNoSuchElementException.java
@@ -3,7 +3,7 @@ package com.elixir.elixir.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "La orden no existe")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No ha sido posible encontrar la orden.")
 public class OrderNoSuchElementException extends Exception {
 
 }
